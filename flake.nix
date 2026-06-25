7eab42c871ac74ad0bce7a246f18c29ea38087ad
matthis-k <matthis.kaelble@gmail.com>
2026-06-25 14:24:23 +0200

    refactor: use flake-parts with module tree

--- flake.nix
+++ flake.nix
@@ -2,48 +2,14 @@
   description = "Phenix cross-repo developer and maintenance tooling";
 
   inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
     phenix-pins.url = "github:matthis-k/phenix-pins";
     nixpkgs.follows = "phenix-pins/nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }: let
-    systems = [ "x86_64-linux" "aarch64-linux" ];
-    forAllSystems = f:
-      builtins.listToAttrs (map (sys: {
-        name = sys;
-        value = f sys;
-      }) systems);
-    pkgsFor = system: import nixpkgs { inherit system; };
-  in {
-    packages = forAllSystems (system: let
-      pkgs = pkgsFor system;
-      tools = pkgs.rustPlatform.buildRustPackage {
-        pname = "phenix-tools";
-        version = "0.1.0";
-        src = ./.;
-        cargoLock.lockFile = ./Cargo.lock;
-      };
-    in {
-      default = tools;
-      sync = tools;
-    });
-
-    apps = forAllSystems (system: let
-      pkgs = pkgsFor system;
-      bin = "${self.packages.${system}.default}/bin/pt";
-    in {
-      sync = {
-        type = "app";
-        program = "${pkgs.writeShellScriptBin "phenix-sync" ''
-          exec ${bin} sync "$@"
-        ''}/bin/phenix-sync";
-      };
-      default = {
-        type = "app";
-        program = "${pkgs.writeShellScriptBin "phenix-tools" ''
-          exec ${bin} "$@"
-        ''}/bin/phenix-tools";
-      };
-    });
-  };
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      imports = [ ./modules ];
+    };
 }
---
+++ modules/default.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, ... }: let
+  tools = pkgs.rustPlatform.buildRustPackage {
+    pname = "phenix-tools";
+    version = "0.1.0";
+    src = ./..;
+    cargoLock.lockFile = ./../Cargo.lock;
+  };
+in {
+  perSystem = {
+    packages.sync = tools;
+    packages.default = tools;
+
+    apps.sync = {
+      type = "app";
+      program = "${pkgs.writeShellScriptBin "phenix-sync" ''
+        exec ${tools}/bin/pt sync "$@"
+      ''}/bin/phenix-sync";
+    };
+    apps.default = {
+      type = "app";
+      program = "${tools}/bin/pt";
+    };
+  };
+}
