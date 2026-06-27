6f01bb6761e7f6bfd6d40b22bbd60f46f74004fd
matthis-k <matthis.kaelble@gmail.com>
2026-06-27 02:10:21 +0200

    feat: flesh out flakeModules.default with phenixWrapped tend/stitch

--- modules/flake-module.nix
+++ modules/flake-module.nix
@@ -1,4 +1,8 @@
-_: {
-  perSystem = _: {
+{ inputs, ... }: {
+  perSystem = { system, ... }: {
+    phenixWrapped = {
+      tend = inputs.phenix-tools.packages.${system}.tend;
+      stitch = inputs.phenix-tools.packages.${system}.stitch;
+    };
   };
 }
